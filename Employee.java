d0abb372ce9f7a7b8945ab914219a1eb2e3706ab
DileepKammila <121295474+DileepKammila@users.noreply.github.com>
2026-06-16 20:52:14 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "John";
+        int age = 25;
+        double salary = 50000.50;
+        boolean isEmployee = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Employee: " + isEmployee);
+    }
+}
